4d0e80e10da3723bea88fa35950b608717b26b51
aV <62926341+a0v0@users.noreply.github.com>
2023-11-01 00:36:43 +0530

    Added code

---
+++ app/page.tsx
@@ -0,0 +1,42 @@
+import { Image } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <div className="sections-container">
+      <section>
+        <Image src={"/1.svg"} objectFit="cover" />
+      </section>
+
+      <section>
+        <Image src={"/2.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/3.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/4.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/5.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/6.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/7.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/8.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/9.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/10.svg"} objectFit="cover" />
+      </section>
+      <section>
+        <Image src={"/11.svg"} objectFit="cover" />
+      </section>
+    </div>
+  );
+}
---
+++ app/section.tsx
@@ -0,0 +1,9 @@
+const Section = ({ src }: any) => {
+  return (
+    <section>
+      <img src={src} alt={`Image`} />
+    </section>
+  );
+};
+
+export default Section;
